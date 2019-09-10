0a687c1a9101a2bbac3d1777e3cd4a0cb5b684f7
Priyesh Vishwakarma <priyesh.vishwakarma@infobeans.com>
2019-09-10 12:19:35 +0530

    added some comment

--- 0601.BlobTransferUI/BlobTransferUI/Program.cs
+++ 0601.BlobTransferUI/BlobTransferUI/Program.cs
@@ -16,6 +16,7 @@ namespace BlobTransferUI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+		//i accidentally added pwd
         }
     }
 }
